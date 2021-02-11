7cfa1b4db37382c4f1d54837eb2ce6c9f54e93b7
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2021-02-10 18:19:45 -0800

    feat(index): add coffee and fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,26 @@ const Index = () => {
   return (
     <>
       <Head>
-        <title>Andrew Brey</title>
+        <title>Andrew Brey - Software Developer</title>
       </Head>
+
+      <div className="flex flex-col items-center justify-center h-screen">
+        <video
+          className="fixed inset-0 h-screen w-screen object-cover"
+          playsInline
+          autoPlay
+          muted
+          loop
+          poster="/assets/coffee.jpg"
+        >
+          <source src="/assets/coffee.webm" type="video/webm" />
+          <source src="/assets/coffee.mp4" type="video/mp4" />
+        </video>
+
+        <div className="relative">
+          <h1 className="text-6xl text-white">Andrew Brey</h1>
+        </div>
+      </div>
     </>
   );
 };
