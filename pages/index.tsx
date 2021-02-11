ff019eb971485f6760a6f2a8095409245c83d3f3
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2021-02-10 17:57:46 -0800

    refactor(*): remove boilerplate for next blog, will come back to that

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,11 @@
 import Head from "next/head";
-import Container from "../components/container";
-import Link from "next/link";
-import Layout from "../components/layout";
-import { CMS_NAME } from "../lib/constants";
-import Post from "../types/post";
-
-type Props = {
-  allPosts: Post[];
-};
 
 const Index = () => {
   return (
     <>
-      <Layout>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
-        <Container>
-          <Link as={`/blog`} href="/blog">
-            <a className="hover:underline">Blog</a>
-          </Link>
-        </Container>
-      </Layout>
+      <Head>
+        <title>Andrew Brey</title>
+      </Head>
     </>
   );
 };
