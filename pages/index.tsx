ea992747c8d323df533b5f30df13f2f670694f3d
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2021-02-10 18:59:51 -0800

    feat(index): implement homepage content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,17 @@
 import Head from "next/head";
+import Link from "next/link";
+
+const links = [
+  { text: "LinkedIn", link: "https://www.linkedin.com/in/andrew-brey" },
+  { text: "Blog", link: "https://blog.andrewbrey.com" },
+  { text: "GitHub", link: "https://github.com/andrewbrey" },
+  {
+    text: "Resume",
+    link:
+      "https://static.fluencyy.com/www.andrewbrey.com/AndrewBrey_resume.pdf",
+  },
+  { text: "Fluencyy LLC", link: "https://www.fluencyy.com" },
+];
 
 const Index = () => {
   return (
@@ -7,7 +20,7 @@ const Index = () => {
         <title>Andrew Brey - Software Developer</title>
       </Head>
 
-      <div className="flex flex-col items-center justify-center h-screen">
+      <div className="p-4 flex flex-col items-center justify-center h-screen">
         <video
           className="fixed inset-0 h-screen w-screen object-cover"
           playsInline
@@ -20,8 +33,24 @@ const Index = () => {
           <source src="/assets/coffee.mp4" type="video/mp4" />
         </video>
 
+        <div className="fixed inset-0 h-screen w-screen bg-smoke-900 opacity-50"></div>
+
         <div className="relative">
-          <h1 className="text-6xl text-white">Andrew Brey</h1>
+          <h1 className="text-5xl md:text-6xl text-smoke-100 font-bold uppercase text-center">
+            Andrew Brey
+          </h1>
+          <h2 className="mt-2 md:mt-3 text-2xl md:text-3xl text-smoke-300 text-center">
+            Software Developer
+          </h2>
+          <div className="mt-12 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4">
+            {links.map((l) => (
+              <Link key={l.text} href={l.link}>
+                <a className="px-3 py-1.5 rounded-full ring-2 ring-smoke-100 uppercase text-sm text-center whitespace-nowrap truncate text-smoke-100 bg-smoke-200 bg-opacity-10 hover:bg-opacity-30 focus:bg-opacity-30 focus:outline-none focus:ring-offset-2">
+                  {l.text}
+                </a>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </>
