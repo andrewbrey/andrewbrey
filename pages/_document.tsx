0d6c62de14713e96b2ab06710b25dab0a2d5be1f
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-03-07 17:01:29 -0800

    feat(links): remove fluencyy link and reformat

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,39 +1,18 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="h-full bg-smoke-900">
         <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/favicon/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon/favicon-16x16.png"
-          />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
           <link rel="manifest" href="/favicon/site.webmanifest" />
-          <link
-            rel="mask-icon"
-            href="/favicon/safari-pinned-tab.svg"
-            color="#5bbad5"
-          />
+          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
           <link rel="shortcut icon" href="/favicon/favicon.ico" />
           <meta name="msapplication-TileColor" content="#00aba9" />
-          <meta
-            name="msapplication-config"
-            content="/favicon/browserconfig.xml"
-          />
+          <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
           <meta name="theme-color" content="#222" />
         </Head>
         <body className="h-full">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
-import Head from "next/head";
-import Link from "next/link";
+import Head from 'next/head';
+import Link from 'next/link';
 
 const links = [
-  { text: "LinkedIn", link: "https://www.linkedin.com/in/andrew-brey" },
-  { text: "Blog", link: "https://blog.andrewbrey.com" },
-  { text: "GitHub", link: "https://github.com/andrewbrey" },
-  { text: "Resume", link: "/assets/AndrewBrey_resume.pdf" },
-  { text: "Fluencyy LLC", link: "https://www.fluencyy.com" },
+  { text: 'LinkedIn', link: 'https://www.linkedin.com/in/andrew-brey' },
+  { text: 'Blog', link: 'https://blog.andrewbrey.com' },
+  { text: 'GitHub', link: 'https://github.com/andrewbrey' },
+  { text: 'Resume', link: '/assets/AndrewBrey_resume.pdf' },
 ];
 
 const Index = () => {
@@ -16,9 +15,9 @@ const Index = () => {
         <title>Andrew Brey - Software Developer</title>
       </Head>
 
-      <div className="p-4 flex flex-col items-center justify-center h-full">
+      <div className="flex flex-col items-center justify-center h-full p-4">
         <video
-          className="fixed inset-0 h-screen w-screen object-cover"
+          className="fixed inset-0 object-cover w-screen h-screen"
           playsInline
           autoPlay
           muted
@@ -29,16 +28,12 @@ const Index = () => {
           <source src="/assets/coffee.mp4" type="video/mp4" />
         </video>
 
-        <div className="fixed inset-0 h-screen w-screen bg-smoke-900 opacity-50"></div>
+        <div className="fixed inset-0 w-screen h-screen opacity-50 bg-smoke-900"></div>
 
         <div className="relative">
-          <h1 className="text-4xl md:text-6xl text-smoke-100 font-bold uppercase text-center">
-            Andrew Brey
-          </h1>
-          <h2 className="mt-2 md:mt-3 text-2xl md:text-3xl text-smoke-300 text-center">
-            Software Developer
-          </h2>
-          <div className="mt-12 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4">
+          <h1 className="text-4xl font-bold text-center uppercase md:text-6xl text-smoke-100">Andrew Brey</h1>
+          <h2 className="mt-2 text-2xl text-center md:mt-3 md:text-3xl text-smoke-300">Software Developer</h2>
+          <div className="grid grid-cols-2 gap-4 mt-12 sm:grid-cols-3 md:grid-cols-5">
             {links.map((l) => (
               <Link key={l.text} href={l.link}>
                 <a className="px-3 py-1.5 rounded-full ring-2 ring-smoke-100 uppercase text-sm text-center whitespace-nowrap truncate text-smoke-100 bg-smoke-200 bg-opacity-10 hover:bg-opacity-30 focus:bg-opacity-30 focus:outline-none focus:ring-offset-2">
