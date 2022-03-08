f3d776ce6e64aa18773aee45be4e6f3f0d91d817
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-03-07 17:11:31 -0800

    feat(*): bump deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: ["./pages/**/*.tsx"],
+  content: ["./pages/**/*.tsx"],
   theme: {
     extend: {
       colors: {
